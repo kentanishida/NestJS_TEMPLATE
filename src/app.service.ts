d30ed163247f8b7bf0b9ef1d8573bbc54ffe1ac8
kentanishida <kentanishida1118@gmail.com>
2023-09-02 20:02:52 +0900

    setup repl

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaClient) {}
+  async getHello(): Promise<string> {
+    return this.prisma.user.findMany()[0] ?? 'hello world';
   }
 }
