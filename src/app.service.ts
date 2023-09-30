0ff64da5979490fd3f2af5252312489fae3f1cbc
kentanishida <kentanishida1118@gmail.com>
2023-09-30 16:27:45 +0900

    seed  流して res で獲得できるまで

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,8 @@ import { PrismaClient } from '@prisma/client';
 export class AppService {
   constructor(private readonly prisma: PrismaClient) {}
   async getHello(): Promise<string> {
-    return this.prisma.user.findMany()[0] ?? 'hello';
+    const res = await this.prisma.user.findMany();
+    console.log('res', res);
+    return res[0].email ?? 'hello';
   }
 }
